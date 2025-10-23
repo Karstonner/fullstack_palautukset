c2f5b2775fd7df3f225836c08890e77a570dca1b
Karstonner <tomi.poutanen@helsinki.fi>
2025-10-23 23:54:07 +0300

    1.6

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const Header = ({ text }) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const headers = ['give feedback', 'statistics']
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {setGood(good + 1)}
+  const handleNeutral = () => {setNeutral(neutral + 1)}
+  const handleBad = () => {setBad(bad + 1)}
+
+  return (
+    <div>
+      <Header text={headers[0]} />
+      <Button onClick={handleGood} text='good' />
+      <Button onClick={handleNeutral} text='neutral' />
+      <Button onClick={handleBad} text='bad' />
+      <Header text={headers[1]} />
+      good {good} <br></br>
+      neutral {neutral} <br></br>
+      bad {bad}
+    </div>
+  )
+}
+
+export default App
