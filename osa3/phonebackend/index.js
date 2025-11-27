257fe005e32acf2e8eff7f2963430d265e6779d9
Karstonner <tomi.poutanen@helsinki.fi>
2025-11-27 08:35:02 +0200

    3.8

--- osa3/phonebackend/index.js
+++ osa3/phonebackend/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-app.use(morgan('tiny'))
 app.use(express.json())
+
+morgan.token('body', (request) => {
+  if (!request.body || Object.keys(request.body).length === 0) return '-'
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let contacts = [
   {
